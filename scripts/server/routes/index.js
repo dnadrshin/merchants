e26783247360b719fd389f5ffa591f9768f7e44f
dimanklg <dimankaliningrad@gmail.com>
2017-12-23 22:51:43 +0200

    update get route  (#11)

--- scripts/server/routes/index.js
+++ scripts/server/routes/index.js
@@ -3,6 +3,14 @@ const
   merchants = require('../fixtures/merchants');
 
 router.get('/merchants', (req, res) => {
+  res.set('X-Pagination-Count', merchants.length);
+  if(req.query.start && req.query.limit) return res.json(merchants.slice(
+    Number(req.query.start) - 1,
+    Number(req.query.start) + Number(req.query.limit) - 1)
+  );
+
+  if(!req.query.start && !req.query.limit) return res.json(merchants);
+  if(req.query.limit) return res.json(merchants.slice(0, req.query.limit));
   return res.json(merchants);
 })
 
